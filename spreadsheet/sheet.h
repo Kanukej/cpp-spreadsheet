e36aeb8ba29a0d6e03f10c037a66fcabaec3b2db
Kanukej <160567252+Kanukej@users.noreply.github.com>
2025-02-14 10:01:32 +0600

    Update sheet.h

--- spreadsheet/sheet.h
+++ spreadsheet/sheet.h
@@ -1,1 +1,53 @@
-/* разместите здесь свой код */
+#pragma once
+
+#include "cell.h"
+#include "common.h"
+
+#include <functional>
+#include <iostream>
+
+class Sheet : public SheetInterface {
+public:
+    Sheet();
+    ~Sheet();
+
+    void SetCell(Position pos, std::string text) override;
+
+    const CellInterface* GetCell(Position pos) const override;
+    CellInterface* GetCell(Position pos) override;
+
+    void ClearCell(Position pos) override;
+
+    Size GetPrintableSize() const override;
+
+    void PrintValues(std::ostream& output) const override;
+    void PrintTexts(std::ostream& output) const override;
+
+	// Можете дополнить ваш класс нужными полями и методами
+
+private:
+    struct ValueVisitor {
+        std::ostream& out;
+        void operator()(const std::string& val) {
+            out << val;
+        }
+        void operator()(const double& val) {
+            out << val;
+        }
+        void operator()(const FormulaError& val) {
+            out << val;
+        }
+    };
+
+    void UpdateSize();
+    bool CheckPosition(const Position& pos) const;
+    
+    template <typename Func>
+    void Print(std::ostream& output, Func pred) const;
+    
+    std::unordered_map<int, std::unordered_set<int>> cols_idx_;
+    std::unordered_map<int, std::unordered_set<int>> rows_idx_;
+    Size size_ {0, 0};
+
+    std::unordered_map<Position, std::unique_ptr<CellInterface>, PositionHash> cells_;
+};
